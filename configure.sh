54fa190cb707bf5a1b20942817f2b33291d97209
Tsvika S <tsvikas@github>
2023-05-24 12:32:29 +0300

    add jupytext to configure.sh

--- configure.sh
+++ configure.sh
@@ -5,5 +5,6 @@ curl -sSL https://install.python-poetry.org | python3 -
 poetry env use python3.11
 poetry install
 poetry run wandb login "$(cat wandb_api_key.secret)"
+poetry run jupytext --sync src/explore_model.py
 echo PYTHON_BIN = "$(poetry env info -p)"/bin/python
 echo CMD = poetry run src/train.py
