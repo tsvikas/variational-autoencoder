d6238655b004fcfbda509272f8b03e6efffc1701
Tsvika S <tsvikas@github>
2023-05-24 14:29:46 +0300

    more configure

--- configure.sh
+++ configure.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-chmod -x -R .
+find -type f -executable | xargs chmod -x
 chmod +x src/train.py ./configure.sh
 sudo apt update
 sudo apt install -y python3.11
@@ -8,6 +8,7 @@ poetry env use python3.11
 poetry install
 poetry run wandb login "$(cat wandb_api_key.secret)"
 poetry run jupytext --sync src/explore_model.py
+poetry run jupyter lab password
 echo PYTHON_BIN = "$(poetry env info -p)"/bin/python
 echo CMD = poetry run src/train.py
 echo JUPYTER = poetry run jupyter lab --no-browser --ip 0.0.0.0
