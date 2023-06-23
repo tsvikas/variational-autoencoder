81440f4dee21ebe7bb1011e940ad534c2cfc0c23
Soof Golan <soofgolan@gmail.com>
2023-06-23 10:13:26 +0300

    implement ConvVAE

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,5 @@
 from .auto_encoder import FullyConnectedAutoEncoder
+from .conv_vae import ConvVAE
 from .mlp import MultiLayerPerceptron
 from .resnet import Resnet
+from .resnet_vae import ResidualAutoencoder
